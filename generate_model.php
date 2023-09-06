039d5d062458003d45ba8af205c429a4230ad081
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2023-09-07 00:07:30 +0500

    Update generate_model.php

--- generate_model.php
+++ generate_model.php
@@ -1,116 +1,11 @@
 <?php
 
 // generate model class
-
+require "trits.php";
 class Model
 {
+    use Sql_codes, Base_codes, Admin_codes;
     public $directory = "models";
-    private $require_base = 'require_once "Base_model.php";';
-    private $extend_base = 'extends Base_model';
-    private $varible_head = "$";
-    private $end_varible = ";";
-    private $varible_value = "=";
-    private $sql_select = 'ses';
-    private $types = [
-        "public",
-        "protected",
-        "private"
-    ];
-    private $tab = "    ";
-    private $probel = " ";
-    private $new_line = "\n";
-    private $class_name = "class";
-    private $function_name = "function";
-    private $close_php = "?>";
-    private $open_php = "<?php";
-    private $close_class = "}";
-    private $open_class = "{";
-    private $get_function = '
-    public function get() {
-          $stmt = $this->db->query("SELECT * FROM $this->model_name");
-          $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-          return $data;
-     }';
-
-    private $update_function = '
-    public function update($data, $id = false)
-    {
-        if ($id) {
-        return "id bolishi kerak";
-        } 
-        $placeholders = [];
-        foreach ($data as $key => $value) {
-            // Escape and sanitize the value
-            $escapedValue = $this->db->quote($value);
-            $placeholders[] = "$key = $escapedValue";
-        }
-        
-        $updateFields = implode(",", $placeholders);
-
-        $sql = "UPDATE $this->model_name SET $updateFields WHERE id = :id";
-
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindValue(":id", $id);
-
-        return $stmt->execute();
-    }';
-
-    private $delete_function = '
-    public function delete($id)
-    {
-        $sql = "DELETE FROM $this->model_name WHERE id = :id";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindValue(":id", $id);
-        return $stmt->execute();
-    }';
-
-    private $insert_function = '
-    public function insert($data)
-    {
-        $placeholders = [];
-        foreach ($data as $key => $value) {
-            // Escape and sanitize the value
-            $escapedValue = $this->db->quote($value);
-            $placeholders[] = "$key = $escapedValue";
-        }
-        
-        $updateFields = implode(",", $placeholders);
-
-        $sql = "INSERT INTO $this->model_name SET $updateFields";
-
-        $stmt = $this->db->prepare($sql);
-
-        return $stmt->execute();
-    }';
-
-    private $select_function = '
-    public function select($id)
-    {
-        $sql = "SELECT * FROM $this->model_name WHERE id = :id";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindValue(":id", $id);
-        $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }';
-
-    private $create_table = '
-    public function create_table($data)
-    {
-        $placeholders = [];
-        foreach ($data as $key => $value) {
-            // Escape and sanitize the value
-            $escapedValue = $this->db->quote($value);
-            $placeholders[] = "$key = $escapedValue";
-        }
-        
-        $updateFields = implode(",", $placeholders);
-
-        $sql = "CREATE TABLE $this->model_name SET $updateFields";
-
-        $stmt = $this->db->prepare($sql);
-
-        return $stmt->execute();
-    }';
 
     public function create_model($classname)
     {
@@ -141,8 +36,42 @@ class Model
         }
         chmod($this->directory . "/" . $classname . ".php", 0755);
     }
+
+
+    public function create_admin($classname, $table_heads = [], $table_values = []){
+        $model_names = $classname . "s";
+        $table_head = "";
+        foreach ($table_heads as $head) {
+            $table_head .= "<th>" . $head . "</th>" . $this->new_line;
+        }
+        $table_value = "";
+        foreach ($table_values as $value) {
+            $table_value .= "'" . $value . "'," . $this->new_line;
+        }
+
+        $compiled =
+        // write php open tag
+        $this->open_php . $this->new_line . $this->new_line . $this->new_line
+        . $this->probel . $this->varible_head . "model_name" . $this->varible_value . "'" . $classname . "'" . $this->end_varible . $this->new_line
+        . $this->probel . $this->varible_head . "model_names" . $this->varible_value . "'" . $model_names . "'" . $this->end_varible . $this->new_line
+        . $this->probel . $this->varible_head . "table_head" . $this->varible_value . '"' . $table_head . '"' . $this->end_varible . $this->new_line
+        . $this->probel . $this->varible_head . "table_values" . $this->varible_value . '[' . $table_value . ']' . $this->end_varible . $this->new_line
+        // write pagination
+        . $this->pagination_code . $this->new_line . $this->close_php
+        // write require base class
+        . $this->get_html . $this->new_line . $this->new_line
+
+
+        // write php close tag
+        . $this->get_footer . $this->new_line . $this->new_line
+        . $this->new_line . $this->close_php;
+    if (file_put_contents($this->directory . "/" . $classname . ".php", $compiled) === FALSE) {
+        throw new Exception("Could not write cache file to path '" . $this->directory . "/" . $classname . ".php" . "'. Is it writable?");
+    }
+    chmod($this->directory . "/" . $classname . ".php", 0755);
+    }
 }
 
 $generate = new Model();
-$generate->directory = "models";
-$generate->create_model("Test");
+$generate->directory = "migrations";
+$generate->create_admin("text", ["id", "title", "text", "active", "create_at"], ["id", "title", "text", "active", "create_at"] );
